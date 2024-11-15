1779061ad9d09e6750ca6562af49f3a921e5ae72
Artur Chemov <arthur.chemov@gmail.com>
2024-11-15 14:29:18 +0200

    fix: styles

--- js/main.js
+++ js/main.js
@@ -1,35 +1,47 @@
-// Testimonials
+document.addEventListener("DOMContentLoaded", () => {
+    initializeTestimonialsSlider();
+    initializeFaqSection();
+});
 
-document.addEventListener("DOMContentLoaded", function () {
+function initializeTestimonialsSlider() {
     const testimonialsWrapper = document.querySelector(".testimonials-wrapper");
     const testimonials = document.querySelectorAll(".testimonial");
     const lines = document.querySelectorAll(".lines .line");
     const prevBtn = document.querySelector(".prev-btn");
     const nextBtn = document.querySelector(".next-btn");
-
+    const prevRect = prevBtn.querySelector("rect");
+    const nextRect = nextBtn.querySelector("rect");
     let currentIndex = 0;
 
-    function updateSlider() {
-        lines.forEach((line) => line.firstElementChild.style.fill = "#C2D1E0");
-
+    const updateSlider = () => {
+        lines.forEach(line => line.firstElementChild.style.fill = "#C2D1E0");
         lines[currentIndex].firstElementChild.style.fill = "#003366";
-
         testimonialsWrapper.style.transform = `translateX(-${currentIndex * 50}%)`;
-    }
 
-    function nextSlide() {
-        currentIndex = (currentIndex + 1) % testimonials.length;
+        if (currentIndex === 0) {
+            prevBtn.disabled = true;
+            prevRect.style.fill = "#C2D1E0";
+        } else {
+            prevBtn.disabled = false;
+            prevRect.style.fill = "#003366";
+        }
+
+        if (currentIndex === testimonials.length - 1) {
+            nextBtn.disabled = true;
+            nextRect.style.fill = "#C2D1E0";
+        } else {
+            nextBtn.disabled = false;
+            nextRect.style.fill = "#003366";
+        }
+    };
+
+    const changeSlide = (indexChange) => {
+        currentIndex = (currentIndex + indexChange + testimonials.length) % testimonials.length;
         updateSlider();
-    }
+    };
 
-    function prevSlide() {
-        currentIndex = (currentIndex - 1 + testimonials.length) % testimonials.length;
-        updateSlider();
-    }
-
-    nextBtn.addEventListener("click", nextSlide);
-
-    prevBtn.addEventListener("click", prevSlide);
+    nextBtn.addEventListener("click", () => changeSlide(1));
+    prevBtn.addEventListener("click", () => changeSlide(-1));
 
     lines.forEach((line, index) => {
         line.addEventListener("click", () => {
@@ -39,30 +51,29 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     updateSlider();
-});
+}
 
+function initializeFaqSection() {
+    const questions = document.querySelectorAll(".question-item-q");
 
-// Faq-section
-window.addEventListener('DOMContentLoaded', () => {
-    const questions = document.querySelectorAll('.question-item-q');
-    questions[0].nextElementSibling.style.display = 'block';
-    questions[0].querySelector('.toggle-minus').style.display = 'block'
-    questions[0].querySelector('.toggle-plus').style.display = 'none'
+    const toggleAnswerVisibility = (question) => {
+        const answer = question.nextElementSibling;
+        const plusIcon = question.querySelector(".toggle-plus");
+        const minusIcon = question.querySelector(".toggle-minus");
+        const isAnswerVisible = answer.style.display === "block";
+
+        answer.style.display = isAnswerVisible ? "none" : "block";
+        plusIcon.style.display = isAnswerVisible ? "block" : "none";
+        minusIcon.style.display = isAnswerVisible ? "none" : "block";
+    };
+
+    if (questions.length > 0) {
+        questions[0].nextElementSibling.style.display = "block";
+        questions[0].querySelector(".toggle-minus").style.display = "block";
+        questions[0].querySelector(".toggle-plus").style.display = "none";
+    }
 
     questions.forEach(question => {
-        question.addEventListener('click', () => {
-            const answer = question.nextElementSibling
-            const plusIcon = question.querySelector('.toggle-plus');
-            const minusIcon = question.querySelector('.toggle-minus');
-
-            const isAnswerVisible = answer.style.display === 'block'
-            answer.style.display = isAnswerVisible ? 'none' : 'block'
-
-            plusIcon.style.display = isAnswerVisible ? 'block' : 'none';
-            minusIcon.style.display = isAnswerVisible ? 'none' : 'block';
-
-
-        })
-    })
-})
-
+        question.addEventListener("click", () => toggleAnswerVisibility(question));
+    });
+}
